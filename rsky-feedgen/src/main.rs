985099013434b442047d20f4c2e2476e495b1d5e
Rudy Fraser <him@rudyfraser.com>
2024-09-05 13:41:59 -0400

    Update Banned_From_TV - soyjak

--- rsky-feedgen/src/main.rs
+++ rsky-feedgen/src/main.rs
@@ -119,7 +119,10 @@ lazy_static! {
         s.insert("did:plc:tquk7ybcb2tvxv6acgqe4q2e"); // HS
         s.insert("did:plc:gyk5exv532seawdowdfwsn2m"); // Anti-Black
         s.insert("did:plc:smmuzxhbumgqptziqeujv2su"); // Anti-Black
-        s.insert("did:plc:vpkthocm76u4rcvw4k2e2l5c"); // Hate Campaign
+        s.insert("did:plc:vpkthocm76u4rcvw4k2e2l5c"); // Hate Campaign (soyjak)
+        s.insert("did:plc:vyxwktjvl4nhybxuirza3l3j"); // Hate Campaign (soyjak)
+        s.insert("did:plc:nynnin6sxmfwgbypwqajyfnk"); // Hate Campaign (soyjak)
+        s.insert("did:plc:dd3seyd2vwpj5a6e7hgactwx"); // Hate Campaign (soyjak)
         s
     };
 }
