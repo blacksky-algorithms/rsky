f9c283b49f8bfea6a5dce0d0b4ae018018274435
Rudy Fraser <him@rudyfraser.com>
2024-09-10 18:17:50 -0400

    Update Banned_From_TV - HS

--- rsky-feedgen/src/main.rs
+++ rsky-feedgen/src/main.rs
@@ -130,6 +130,7 @@ lazy_static! {
         s.insert("did:plc:rrygeg5e5sze75absmolbibm"); // Hate Campaign (soyjak)
         s.insert("did:plc:sysv2njdvrhv2i7j2jtmudnn"); // Hate Campaign (soyjak)
         s.insert("did:plc:lqkz7god5gbb53isjvxk7g5n"); // Hate Campaign (soyjak)
+        s.insert("did:plc:cco7fsmvzh4mwouwnixgrice"); // HS
         s
     };
 }
