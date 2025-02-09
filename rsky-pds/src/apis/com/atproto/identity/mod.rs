32acc9e8071d88a1e60434e68b0f9929d35bbd54
Ripperoni <email@ripperoni.com>
2025-02-09 14:02:56 -0800

    Implemented Request PLC Signature Call

--- rsky-pds/src/apis/com/atproto/identity/mod.rs
+++ rsky-pds/src/apis/com/atproto/identity/mod.rs
@@ -1,2 +1,3 @@
 pub mod resolve_handle;
 pub mod update_handle;
+pub mod request_plc_operation;
---
+++ rsky-pds/src/apis/com/atproto/identity/request_plc_operation.rs
@@ -0,0 +1,42 @@
+use crate::account_manager::helpers::account::AvailabilityFlags;
+use crate::account_manager::AccountManager;
+use crate::apis::ApiError;
+use crate::auth_verifier::AccessFull;
+use crate::mailer::{send_plc_operation, TokenParam};
+use crate::models::models::EmailTokenPurpose;
+
+#[rocket::post("/xrpc/com.atproto.identity.requestPlcOperationSignature")]
+#[tracing::instrument(skip_all)]
+pub async fn request_plc_operation_signature(auth: AccessFull) -> Result<(), ApiError> {
+    let requester = auth.access.credentials.unwrap().did.unwrap();
+    let availability_flags = AvailabilityFlags {
+        include_taken_down: Some(true),
+        include_deactivated: Some(true),
+    };
+    let account = AccountManager::get_account(&requester, Some(availability_flags))
+        .await?
+        .expect("Account not found despite valid access");
+
+    let token;
+    match AccountManager::create_email_token(&requester, EmailTokenPurpose::PlcOperation).await {
+        Ok(res) => {
+            token = res;
+        }
+        Err(error) => {
+            tracing::error!("Failed to create plc operation token\n{error}");
+            return Err(ApiError::RuntimeError);
+        }
+    }
+
+    match send_plc_operation(account.email.unwrap(), TokenParam { token }).await {
+        Ok(_) => {
+            tracing::info!("Successfully sent PLC Operation Email");
+        }
+        Err(error) => {
+            tracing::error!("Failed to send PLC Operation Token Email\n{error}");
+            return Err(ApiError::RuntimeError)
+        }
+    }
+
+    Ok(())
+}
