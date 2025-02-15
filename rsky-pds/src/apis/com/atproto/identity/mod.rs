8ddf839d2618209f8d56b63184e5a6eb037dc429
Ripperoni <email@ripperoni.com>
2025-02-15 09:57:07 -0800

    Ran rustfmt

--- rsky-pds/src/apis/com/atproto/identity/mod.rs
+++ rsky-pds/src/apis/com/atproto/identity/mod.rs
@@ -1,5 +1,5 @@
-pub mod request_plc_operation_signature;
 pub mod get_recommended_did_credentials;
+pub mod request_plc_operation_signature;
 pub mod resolve_handle;
 pub mod sign_plc_operation;
 pub mod update_handle;
