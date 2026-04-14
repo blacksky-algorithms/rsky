a44707f762bf59293adba50340932a49b623da02
sean watters <sean@watters.io>
2026-04-13 20:12:02 -0700

    properly derefs the LazyLocks

--- rsky-pds/src/main.rs
+++ rsky-pds/src/main.rs
@@ -2,9 +2,9 @@ use rsky_pds::build_rocket;
 
 #[rocket::main]
 async fn main() {
-    let _ = &rsky_pds::context::PDS_REPO_SIGNING_KEYPAIR;
-    let _ = &rsky_pds::auth_verifier::PDS_JWT_KEYPAIR;
-    let _ = &rsky_pds::apis::com::atproto::server::PDS_PLC_ROTATION_KEYPAIR;
+    let _ = &*rsky_pds::context::PDS_REPO_SIGNING_KEYPAIR;
+    let _ = &*rsky_pds::auth_verifier::PDS_JWT_KEYPAIR;
+    let _ = &*rsky_pds::apis::com::atproto::server::PDS_PLC_ROTATION_KEYPAIR;
 
     let subscriber = tracing_subscriber::FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber).unwrap();
